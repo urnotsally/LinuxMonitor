f5cd609ad98869c716d2b95456196c84a92681ad
yqw <yuqianwen@hikvision.com>
2019-08-05 19:50:24 +0800

    调整输出格式

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         print "[cpu_occupy] "+r'% '+": %.2f" % cpu_occupy.get_cpu_occupy()
         print "[memory_occupy]"+r' % '+": %.2f" % memory_occupy.get_mem_occupy()
         load = cpu_load.get_cpu_load()
-        print "[cpu_load] %s %s %s" % (load.one, load.five, load.fifteen)
+        print "[cpu_load] 1min 5min 15mim: %s %s %s" % (load.one, load.five, load.fifteen)
         for item in get_net_rate():
             print "[%s] receive: %.2f KB/S ; transmit: %.2f KB/S" % (item.interface, item.recv_rate, item.transmit_rate)
         for dev in disk_usage.disk_partitions():
